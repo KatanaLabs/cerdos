df38d07a26c3f690fa47ea58b12310fd94c2d3b5
Kevin Jimenez <kevinjimenezlorenzo@gmail.com>
2022-01-16 08:55:46 -0400

    [ADD] app vw_pos_receipt_table

---
+++ vw_pos_receipt_table/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'POS Receipt Table Theme',
+    'summary': 'Table template for customer receipt in PoS module.',
+    'description': '',
+    'author': 'ViraWeb',
+    'website': 'https://viraweb123.ir',
+    'category': 'Point of Sale',
+    'version': '14.0.0',
+    'depends': ['base', 'point_of_sale'],
+    'qweb': ['static/src/xml/pos_order_line.xml'],
+    'data': [],
+    'images': ['static/description/images/main_screenshot.png'],
+    'license': 'LGPL-3',
+    'auto_install': False,
+    'installable': True,
+}
